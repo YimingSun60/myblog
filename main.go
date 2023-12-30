3e8cdc348fe412d23464e499291419af7d9fe433
YimingSun60 <ysun60@toki.waseda.jp>
2023-12-31 05:11:19 +0900

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println("Start the service")
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.Handle(assetsPath, http.StripPrefix(assetsPath, http.FileServer(http.Dir("templates/assets"))))
+	http.Handle(assetsPath, http.StripPrefix(assetsPath, http.FileServer(http.Dir("./templates/assets"))))
 	err := http.ListenAndServe(portNumber, nil)
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %w", err))

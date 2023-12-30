de90bb728e66344d59f102d1a9fad1320dcbf6f1
YimingSun60 <ysun60@toki.waseda.jp>
2023-12-31 02:32:31 +0900

    rename the html file

--- handlers.go
+++ handlers.go
@@ -5,9 +5,9 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.gohtml")
+	renderTemplate(w, "home.page.html")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.gohtml")
+	renderTemplate(w, "about.page.html")
 }

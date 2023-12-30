95756c14eb91abc0582cdde870ed58b7d3cbb459
YimingSun60 <ysun60@toki.waseda.jp>
2023-12-30 17:11:22 +0900

    update jenkin pipline

--- main.go
+++ main.go
@@ -5,31 +5,21 @@ import (
 	"net/http"
 )
 
-/*func readVariable() (string, string, string, string) {
-	viper.SetConfigName("default")
-	viper.SetConfigType("json")
-	viper.AddConfigPath("/etc")
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil {             // Handle errors reading the config file
-		panic(fmt.Errorf("fatal error config file: %w", err))
-	}
-	cer := viper.GetString("server.certificate")
-	key := viper.GetString("server.key")
-	port := viper.GetString("server.port")
-	addr := viper.GetString("server.address")
-	return cer, key, port, addr
-}*/
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the Home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the About page")
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
 
 func main() {
-	fmt.Println("Welcome")
-	//cer, key, port, addr := readVariable()
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello world")
-		if err != nil {
-			panic(fmt.Errorf("fatal error config file: %w", err))
-		}
-		fmt.Printf("Number of bytes written: %d", n)
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
